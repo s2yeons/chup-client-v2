ab37d2b48d054010260d480af7ae35d2718e2fdf
seoxeon09 <s25037@gsm.hs.kr>
2026-07-27 22:51:50 +0900

    add(user): 로그인 유저 정보 조회 엔티티 추가

---
+++ packages/core/src/entities/user/model/types.ts
@@ -0,0 +1,12 @@
+export type UserRoleType = 'STUDENT' | 'ADMIN';
+
+export interface UserType {
+  id: number;
+  role: UserRoleType;
+  studentId: string | null;
+  name: string;
+  email: string;
+  phoneNumber: string | null;
+  approved: boolean;
+  createdAt: string;
+}
