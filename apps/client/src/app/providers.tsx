aa97727133b7fb6fdae7b92c03fd65cc13765570
seoxeon09 <s25037@gsm.hs.kr>
2026-07-29 14:15:46 +0900

    refactor(global): 미사용 recruitment 목업 엔티티 제거

--- apps/client/src/app/providers.tsx
+++ apps/client/src/app/providers.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 
-import { RecruitmentProvider } from '@chup/core/entities';
 import { ThemeProvider, Toaster } from '@chup/ui';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -29,7 +28,7 @@ const Providers = ({ children }: ProvidersProps) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <QueryClientProvider client={queryClient}>
-        <RecruitmentProvider>{children}</RecruitmentProvider>
+        {children}
         <ReactQueryDevtools initialIsOpen={false} />
         <Toaster position="top-center" richColors />
       </QueryClientProvider>
--- packages/core/src/entities/index.ts
+++ packages/core/src/entities/index.ts
@@ -1,2 +1,1 @@
-export * from './recruitment';
 export * from './user';
